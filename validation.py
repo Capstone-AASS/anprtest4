0bf219f44807632d3dfc2c0e65c214af9df88a31
Samarjeet09 <singhsamarjeet09@gmail.com>
2024-04-23 10:30:00 +0530

    done training for 4 iters

--- validation.py
+++ validation.py
@@ -2,14 +2,13 @@ from ultralytics import YOLO
 
 import os
 
-if __name__ == "__main__fv":
+if __name__ == "__main__":
     # Load a model 
     model = YOLO("./best.pt")
 
     # Customize validation settings
     validation_results = model.val(data='config.yaml',
                                imgsz=640,
-                               batch=16,
                                conf=0.25,
                                iou=0.6,
                                device='0')
