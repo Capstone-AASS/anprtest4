5270e52f54e9d528b0d48ae3e4f843d44be4c8a5
CHIRAG <cgupta_be21@thapar.edu>
2024-12-03 10:40:42 +0530

    feed started

--- my-app/src/pages/LiveFeedsPage.js
+++ my-app/src/pages/LiveFeedsPage.js
@@ -1,10 +1,10 @@
 import React, { useState } from 'react'
-import LiveFeeds from '../components/feed/LiveFeeds';
+// import LiveFeeds from '../components/feed/LiveFeeds';
 import { Box } from '@mui/material';
 import Sidebar from '../components/common/Sidebar';
 import Header from '../components/common/Header';
 import Footer from '../components/common/Footer';
-// import LiveFeeds from '../components/feed/Feed2';
+import LiveFeeds from '../components/feed/Feed2';
 
 function LiveFeedsPage() {
     const [isSidebarOpen, setIsSidebarOpen] = useState(true);
